029ffcae3035ba0335141953c3e98a87e6fd6ff6
muhammadiftikhar <Iftikhar1>
2020-11-12 15:39:01 +0500

    frontend example ajax call added

--- Backend/models/board.model.js
+++ Backend/models/board.model.js
@@ -5,6 +5,7 @@ var mongoose = require('mongoose'),
 
 var BoardSchema = new Schema({
     board: { type: String, required: true, index: { unique: true } },
+    game_id: { type: Number, default: 0},
 });
 
 BoardSchema.methods.getBoardObject = function(cb) {
@@ -12,5 +13,15 @@ BoardSchema.methods.getBoardObject = function(cb) {
     cb(null, JSON.parse(this.board));
 };
 
-
 module.exports = mongoose.model('board', BoardSchema);
+
+BoardSchema.methods.getBoardObject = function(cb,game_id) {
+
+    cb(null, JSON.parse(this.board));
+};
+
+BoardSchema.methods.getBoardObject = function(cb,game_id) {
+
+    cb(null, JSON.parse(this.board));
+};
+

f1fc8bc674bcd7446090d7218d14d23ecb301ff9
adnan-khan1122 <ashafiq@mun.ca>
2020-12-01 21:33:39 -0330

    user authentication function added

--- Backend/controller/updated/authentication.js
+++ Backend/controller/updated/authentication.js
@@ -2,3 +2,54 @@ User = require('../models/user.model');
 Friends = require('../models/friends.model');
 
 // Login function
+exports.loginUser = (req, res) => {
+
+    let userName = req.param('user_name');
+    let password = req.param('password');
+
+    User.findOne({username: userName}, function (err, user) {
+        console.log(user);
+        console.log(err);
+
+        if (err) {
+            res.status(500).send({
+                message: err.message
+            });
+            return;
+        }
+
+        if (user === undefined || user === null) {
+            res.status(401).send({
+                message: "Incorrect username"
+            });
+            return;
+        }
+
+        user.comparePassword(password, function (err, isMatch) {
+            if (err || !isMatch) {
+                res.status(401).send({
+                    message: "Your Password mismatch. Please enter a correct password"
+                });
+                return;
+            } else {
+                Friends.find({user_id: user.id}, function (err, friends) {
+                    if (err) {
+                        res.status(401).send({
+                            message: "Unable to fetch friends"
+                        });
+                        return;
+                    }
+
+                    res.status(200).send({
+                        data: {"profile": user, 'friends': friends, "test": true},
+                        message: "you are successfully logged In"
+                    });
+
+                });
+
+
+            }
+        });
+    });
+
+};

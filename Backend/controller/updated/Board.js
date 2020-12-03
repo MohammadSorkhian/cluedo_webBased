091045ad023fb0bd487e00a23e5fb3123f760e70
adnan-khan1122 <ashafiq@mun.ca>
2020-12-03 01:39:24 -0330

    setting for card, weapons and board added

--- Backend/controller/updated/Board.js
+++ Backend/controller/updated/Board.js
@@ -26,7 +26,10 @@ class Board {
             }
         }
     }
-// SET UP STUDY ROOM
+
+    generateNewBoard(numberOfPlayers,players){
+
+        // SET UP STUDY ROOM
         let study = new Room('STUDY',0,3,0,6,4,6);
         for(let r=study.startRowIndex;r<=study.endRowIndex;r++){
             for(let c=study.startColumnIndex;c<=study.endColumnIndex;c++){
@@ -34,15 +37,15 @@ class Board {
             }
         }
 
-       // SET UP HALL ROOM
+        // SET UP HALL ROOM
         let hall = new Room('HALL',0,6,9,14,7,12);
         for(let r=hall.startRowIndex;r<=hall.endRowIndex;r++){
             for(let c=hall.startColumnIndex;c<=hall.endColumnIndex;c++){
                 this.BOARD[r][c] = hall;
             }
         }
-		
-		// SET UP LOUNGE ROOM
+
+        // SET UP LOUNGE ROOM
         let lounge = new Room('LOUNGE',0,4,17,23,5,23);
         for(let r=lounge.startRowIndex;r<=lounge.endRowIndex;r++){
             for(let c=lounge.startColumnIndex;c<=lounge.endColumnIndex;c++){
@@ -80,8 +83,8 @@ class Board {
         }
 
         this.BOARD[19][5] =  new Tile();
-		
-		// SET UP DINNING ROOM
+
+        // SET UP DINNING ROOM
         let dinning = new Room('DINNING',8,15,17,23,15,18);
         for(let r=dinning.startRowIndex;r<=dinning.endRowIndex;r++){
             for(let c=dinning.startColumnIndex;c<=dinning.endColumnIndex;c++){
@@ -121,10 +124,145 @@ class Board {
 
         this.BOARD[23][14] =  new Tile();
         this.BOARD[23][15] =  new Tile();
-		
-    generateNewBoard(numberOfPlayers,players){
 
-       
+        // SET CARDS
+        let roomCards = [];
+        let characterCards = [];
+        let weaponCards = [];
+
+        // setting rooms
+
+        let studyCard = new Card.Card('STUDY',Card.ROOM);
+        roomCards.push(studyCard);
+
+        let hallCard = new Card.Card('HALL',Card.ROOM);
+        roomCards.push(hallCard);
+
+        let loungCard = new Card.Card('LOUNGE',Card.ROOM);
+        roomCards.push(loungCard);
+
+        let libraryCard = new Card.Card('LIBRARY',Card.ROOM);
+        roomCards.push(libraryCard);
+
+        let billiardCard = new Card.Card('BILLIARD',Card.ROOM);
+        roomCards.push(billiardCard);
+
+        let consdrvatoryCard = new Card.Card('CONSDRVATORY',Card.ROOM);
+        roomCards.push(consdrvatoryCard);
+
+        let dinningCard = new Card.Card('DINNING',Card.ROOM);
+        roomCards.push(dinningCard);
+
+        let kitchenCard = new Card.Card('KITCHEN',Card.ROOM);
+        roomCards.push(kitchenCard);
+
+        let ballRoomCard = new Card.Card('BALLROOM',Card.ROOM);
+        roomCards.push(ballRoomCard);
+
+        // setting characters
+
+        let colonelMustardCard = new Card.Card('Colonel Mustard',Card.CHARACTER);
+        characterCards.push(colonelMustardCard);
+
+        let missScarletCard = new Card.Card('Miss Scarlet',Card.CHARACTER);
+        characterCards.push(missScarletCard);
+
+        let professorPlumCard = new Card.Card('Professor Plum',Card.CHARACTER);
+        characterCards.push(professorPlumCard);
+
+        let mrGreenCard = new Card.Card('Mr Green',Card.CHARACTER);
+        characterCards.push(mrGreenCard);
+
+        let mrsWhiteCard = new Card.Card('Mrs. White',Card.CHARACTER);
+        characterCards.push(mrsWhiteCard);
+
+        let mrsPeacockCard = new Card.Card('Mrs. Peacock',Card.CHARACTER);
+        characterCards.push(mrsPeacockCard);
+
+        // setting weapons
+
+        let candleStickCard = new Card.Card('Candle Stick',Card.WEAPON);
+        weaponCards.push(candleStickCard);
+
+        let ropeCard = new Card.Card('Rope',Card.WEAPON);
+        weaponCards.push(ropeCard);
+
+        let knifeCard = new Card.Card('Knife',Card.WEAPON);
+        weaponCards.push(knifeCard);
+
+        let leadPipeCard = new Card.Card('Lead Pipe',Card.WEAPON);
+        weaponCards.push(leadPipeCard);
+
+        let wrenchCard = new Card.Card('Wrench',Card.WEAPON);
+        weaponCards.push(wrenchCard);
+
+        let revolverCard = new Card.Card('Revolver',Card.WEAPON);
+        weaponCards.push(revolverCard);
+
+
+
+        let AllCards = weaponCards.concat(characterCards).concat(roomCards);
+        this.CARDS = AllCards;
+
+
+        let envelopIndexWeapon = Board.getRandomInt(6);
+        let envelopIndexRoom = Board.getRandomInt(9);
+        let envelopIndexChar = Board.getRandomInt(6);
+
+
+        // SETTING ENVELOP ON BOARD
+        let envalop = new Envalop(weaponCards[envelopIndexWeapon],roomCards[envelopIndexRoom],characterCards[envelopIndexChar],
+            8,14,9,13);
+
+        for(let r=envalop.startRowIndex;r<=envalop.endRowIndex;r++){
+            for(let c=envalop.startColumnIndex;c<=envalop.endColumnIndex;c++){
+                this.BOARD[r][c] = envalop;
+            }
+        }
+
+        // delete envelop cards from arrays
+        weaponCards.splice(envelopIndexWeapon, 1);
+        characterCards.splice(envelopIndexChar,1);
+        roomCards.splice(envelopIndexRoom,1);
+
+
+        // shuffling cards and assign cards to players
+
+        AllCards = weaponCards.concat(characterCards).concat(roomCards);
+
+
+        AllCards = this.shuffle(AllCards)
+        console.log(AllCards)
+        var i=0
+        while(AllCards.length!=0){
+            if (i % players.length == 0){
+                i = 0
+            }
+            players[i]['cards'].push(AllCards[0])
+            AllCards.splice(0,1)
+            i++
+        }
+
+        for(let y=0;y<players.length;y++) {
+
+            console.log(players[y].playerCharacterObject.name);
+
+            for(let x=0;x<Character.CharacterSpec.length;x++){
+
+                if(Character.CharacterSpec[x].name === players[y].playerCharacterObject.name) {
+
+                    console.log(Character.CharacterSpec[x].name);
+                    console.log(Character.CharacterSpec[x].position[1]);
+                    console.log(Character.CharacterSpec[x].position[0]);
+
+                    console.log(players[y])
+
+                    this.BOARD[Character.CharacterSpec[x].position[1]][Character.CharacterSpec[x].position[0]].player = players[y];
+
+                    break;
+                }
+            }
+        }
 
     }
 

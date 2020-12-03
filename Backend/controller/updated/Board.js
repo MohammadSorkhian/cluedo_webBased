8ae66982f71ea3c230285aad1b5770a456f726a4
adnan-khan1122 <ashafiq@mun.ca>
2020-12-03 00:25:55 -0330

    BILLIARD Room and CONSDRVATORY Room added

--- Backend/controller/updated/Board.js
+++ Backend/controller/updated/Board.js
@@ -34,13 +34,53 @@ class Board {
             }
         }
 
-        // SET UP HALL ROOM
+ // SET UP HALL ROOM
         let hall = new Room('HALL',0,6,9,14,7,12);
         for(let r=hall.startRowIndex;r<=hall.endRowIndex;r++){
             for(let c=hall.startColumnIndex;c<=hall.endColumnIndex;c++){
                 this.BOARD[r][c] = hall;
             }
         }
+		
+		// SET UP LOUNGE ROOM
+        let lounge = new Room('LOUNGE',0,4,17,23,5,23);
+        for(let r=lounge.startRowIndex;r<=lounge.endRowIndex;r++){
+            for(let c=lounge.startColumnIndex;c<=lounge.endColumnIndex;c++){
+                this.BOARD[r][c] = lounge;
+            }
+        }
+
+        // SET UP LIBRARY ROOM
+        let library = new Room('LIBRARY',6,10,0,6,10,6);
+        for(let r=library.startRowIndex;r<=library.endRowIndex;r++){
+            for(let c=library.startColumnIndex;c<=library.endColumnIndex;c++){
+                this.BOARD[r][c] = library;
+            }
+        }
+
+        this.BOARD[6][6] =  new Tile();
+        this.BOARD[10][6] =  new Tile();
+
+
+
+        // SET UP BILLIARD ROOM
+        let billiard = new Room('BILLIARD',12,16,0,5,13,6);
+        for(let r=billiard.startRowIndex;r<=billiard.endRowIndex;r++){
+            for(let c=billiard.startColumnIndex;c<=billiard.endColumnIndex;c++){
+                this.BOARD[r][c] = billiard;
+            }
+        }
+
+        // SET UP CONSDRVATORY ROOM
+        let conservatory = new Room('CONSERVATORY',19,23,0,5,19,5);
+        for(let r=conservatory.startRowIndex;r<=conservatory.endRowIndex;r++){
+            for(let c=conservatory.startColumnIndex;c<=conservatory.endColumnIndex;c++){
+                this.BOARD[r][c] = conservatory;
+            }
+        }
+
+        this.BOARD[19][5] =  new Tile();
+		
     generateNewBoard(numberOfPlayers,players){
 
        
